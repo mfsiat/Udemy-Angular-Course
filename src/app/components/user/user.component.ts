f7568776030611c2dc493d172da7a56b0f2f4048
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-10-23 13:46:59 +0600

    How to separate the markup from the ts file

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-user',
-  template: '<h2>John Doe</h2>'
+  templateUrl: './user.component.html'
+  // template: '<h2>John Doe</h2>'
 })
 
 export class UserComponet {
