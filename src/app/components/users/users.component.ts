185f2698e2cc80f018693d566db8e18d4a5a3080
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-19 18:23:35 +0600

    loops with ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: User[];
   constructor() { }
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 26,
+        address: {
+          street: '214/B',
+          city: 'Dhaka',
+          country: 'Bangladesh'
+        }
+      },
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 26,
+        address: {
+          street: '214/B',
+          city: 'Dhaka',
+          country: 'Bangladesh'
+        }
+      },
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 26,
+        address: {
+          street: '214/B',
+          city: 'Dhaka',
+          country: 'Bangladesh'
+        }
+      }
+    ]
   }
 
 }
